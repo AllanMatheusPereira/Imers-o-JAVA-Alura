aa9c4009332c9b89ca0ded31cb1e19466bd649f5
Allan Matheus Pereira <allan.m.pereira@outlook.com>
2023-03-28 13:57:19 -0300

    Criando response request  e a  classe JsonParser

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -17,7 +19,10 @@ public class App {
         System.out.println(body);
 
         //Pegar só os dados que interessam (Titulo, poster(imagem) e classificação(ranking))
+        var Parser = new JsonParser();
+        List<Map<String,String>>listaDeFilmes = parser.parse(body);
 
+        
         //Exibir e manipular os dados
 
     }
